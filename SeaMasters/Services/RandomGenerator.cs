b4eeec9c9ad870afb3fed21a3262deeb920a5a57
Monika Krella <monika.krella@gmail.com>
2022-06-04 18:16:45 +0200

    created unit tests for shooting board methods, random  generator methods and one test for game manager

--- SeaMasters/Services/RandomGenerator.cs
+++ SeaMasters/Services/RandomGenerator.cs
@@ -7,7 +7,7 @@ namespace SeaMasters.Services;
 public class RandomGenerator
 {
     private static readonly Random Random = Random.Shared;
-    
+
     public static Coordinates GetRandomCoords()
     {
         int x = Random.Next(0, GameSettings.BOARD_DIMENSION);
@@ -18,15 +18,19 @@ public class RandomGenerator
     public static Coordinates GetAdjacentCoords(Coordinates currentCoords)
     {
         int xMinValue = currentCoords.X - 1 >= 0 ? currentCoords.X - 1 : 0;
-        int xMaxValue = currentCoords.X + 1 < GameSettings.BOARD_DIMENSION ? currentCoords.X + 1 : GameSettings.BOARD_LAST_INDEX;
+        int xMaxValue = currentCoords.X + 1 < GameSettings.BOARD_DIMENSION
+            ? currentCoords.X + 1
+            : GameSettings.BOARD_LAST_INDEX;
         int yMinValue = currentCoords.Y - 1 >= 0 ? currentCoords.Y - 1 : 0;
-        int yMaxValue = currentCoords.Y + 1 < GameSettings.BOARD_DIMENSION ? currentCoords.Y + 1 : GameSettings.BOARD_LAST_INDEX;
-        
-        int x = Random.Next(xMinValue, xMaxValue+1);
-        int y = Random.Next(yMinValue, yMaxValue+1);
+        int yMaxValue = currentCoords.Y + 1 < GameSettings.BOARD_DIMENSION
+            ? currentCoords.Y + 1
+            : GameSettings.BOARD_LAST_INDEX;
+
+        int x = Random.Next(xMinValue, xMaxValue + 1);
+        int y = Random.Next(yMinValue, yMaxValue + 1);
         return new Coordinates(x, y);
     }
-    
+
     public static Direction GetDirection()
     {
         var directions = Enum.GetValues(typeof(Direction));
--- SeaMastersTests/ShootingBoardTests.cs
+++ SeaMastersTests/ShootingBoardTests.cs
@@ -1,5 +1,3 @@
-using System.Reflection;
-using SeaMasters;
 using SeaMasters.Enums;
 using SeaMasters.Models;
 
@@ -8,18 +6,29 @@ namespace SeaMastersTests;
 public class ShootingBoardTests
 {
     private ShootingBoard shootingBoard;
+
     public ShootingBoardTests()
     {
         shootingBoard = new ShootingBoard();
     }
-    
+
     [Fact]
     public void UpdateField_ShouldChangeStateAtCurrentFieldCoords()
     {
         var expected = FieldStateType.Hit;
-        shootingBoard.UpdateField(new Coordinates(5,5), FieldStateType.Hit);
+        shootingBoard.UpdateField(new Coordinates(5, 5), FieldStateType.Hit);
         var actual = shootingBoard.ShootingArea[5][5];
-        
+
+        Assert.Equal(expected, actual);
+    }
+
+
+    [Fact]
+    public void UpdateAfterDestruction_ShouldRetutnAllEmptyFieldsAround()
+    {
+        var expected = new HashSet<Coordinates>
+            { new(5, 6), new(7, 6), new(5, 5), new(6, 5), new(7, 5), new(5, 7), new(6, 7), new(7, 7) };
+        var actual = shootingBoard.UpdateAfterDestruction(new Coordinates(6, 6));
         Assert.Equal(expected, actual);
     }
 }
